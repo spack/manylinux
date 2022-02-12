86ff30b8936ab4dc06edd1be265f12a3d358e580
mayeut <mayeut@users.noreply.github.com>
2022-02-12 18:36:57 +0100

    fix publication

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-tag="quay.io/pypa/${POLICY}_poc_${PLATFORM}"
+tag="quay.io/pypa/${POLICY}_${PLATFORM}"
+tag_poc="quay.io/pypa/${POLICY}_poc_${PLATFORM}"
 build_id=$(git show -s --format=%cd-%h --date=short ${COMMIT_SHA})
 
 docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-docker tag ${tag}:${COMMIT_SHA} ${tag}:poc
-docker push ${tag}:poc
+docker tag ${tag}:${COMMIT_SHA} ${tag_poc}:poc
+docker push ${tag_poc}:poc
